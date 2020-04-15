e5e0b87635df5b470595e94adba0a51a52a41e2d
Jenny Tran <jenny.thuy.dung.tran@post.au.dk>
2020-04-15 20:21:20 +0200

    slettet

--- Assignment2_Morgenmadsbuffeten/Models/ApplicationUser.cs
+++ Assignment2_Morgenmadsbuffeten/Models/ApplicationUser.cs
@@ -1,14 +1,14 @@
-﻿using Microsoft.AspNetCore.Identity;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 
 namespace Assignment2_Morgenmadsbuffeten.Models
 {
-    public class ApplicationUser :IdentityUser
-    { 
+    public class ApplicationUser : IdentityUser
+    {
         [Required]
         [PersonalData]
         public string Name { get; set; }
